71596d380859b48e0842fea06b384c792e940e98
mariviit <mariviit@greeny.cs.tlu.ee>
2016-09-14 09:35:58 +0300

    added something

--- helloworld.php
+++ helloworld.php
@@ -9,3 +9,47 @@
 	echo $firstName." ".$lastName;
 	
 ?>
+
+
+<br>
+
+<?php
+
+	$age = 5;
+	
+	if ($age < 18) {
+		
+		//kui vastus on tõene (true)
+		echo "alaealine";
+		
+	} else {
+		
+		//kui vastus on väärtuse (false)
+		echo "täisealine";
+	}
+
+?>
+
+<br>
+
+
+<?php
+
+
+	for ($i = 0; $i < $age; $i = $i + 1) {
+		
+		echo $i."palju ";
+		
+	
+	}
+	echo "õnne";
+
+
+
+?>
+
+<br>
+<?php
+	echo date("d.l.F.Y.g.i.s");
+	
+?>
